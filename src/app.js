cbd1cd1eec880fa6d43f090a9385a87a39ba246b
Eunseo Kim <kim.eunseo@kakao.com>
2022-08-27 23:24:09 +0900

    개발 환경 세팅

---
+++ src/app.js
@@ -0,0 +1,7 @@
+export default function App() {
+  const app = document.querySelector('.app');
+  const template = `
+        <h1>Hello World</h1>
+    `;
+  app.innerHTML = template;
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    hot: true,
+  },
+  devtool: 'source-map',
+};
